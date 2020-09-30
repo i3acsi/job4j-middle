812ec250be6bd7afae5247225158068a341f72f0
i3acsi <gasevskyv@gmail.com>
2020-09-30 10:38:13 +0700

    4.Thread without shared resources

---
+++ src/main/java/ru/job4j/concurrent/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.concurrent;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String name;
+
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+}
