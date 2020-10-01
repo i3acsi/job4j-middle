b4c5b3452af9336943182d52661e40664546f61b
i3acsi <gasevskyv@gmail.com>
2020-10-01 11:13:36 +0700

    1. Visibility.

--- src/main/java/ru/job4j/concurrent/ParseFile.java
+++ src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -1,61 +1,116 @@
 package ru.job4j.concurrent;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class ParseFile {
+    private static ConcurrentMap<File, ParseFile> files = new ConcurrentHashMap<>();
+    private File file;
 
-    private ParseFile() {
+    public static ParseFile of(File file) {
+        return files.computeIfAbsent(file, k -> new ParseFile(file));
     }
 
-    public static String getContent(String file) {
-        synchronized (ParseFile.class) {
-            StringBuilder result = new StringBuilder();
-            try (FileChannel channel = FileChannel.open(Path.of(file), StandardOpenOption.READ)) {
-                int bufferSize = 1024;
-                if (bufferSize > channel.size()) {
-                    bufferSize = (int) channel.size();
+    private ParseFile(File file) {
+        this.file = file;
+    }
+
+    public String getContent() {
+        if (this.file.exists()) {
+            synchronized (this) {
+                StringBuilder result = new StringBuilder();
+                try (FileChannel channel = FileChannel.open(this.file.toPath(), StandardOpenOption.READ)) {
+                    int bufferSize = 1024;
+                    if (bufferSize > channel.size()) {
+                        bufferSize = (int) channel.size();
+                    }
+                    ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+                    while (channel.read(buffer) > 0) {
+                        buffer.limit(buffer.position());
+                        buffer.rewind();
+                        result.append(StandardCharsets.UTF_8.decode(buffer).toString());
+                        buffer.clear();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
-                while (channel.read(buffer)>0){
-                    buffer.limit(buffer.position());
-                    buffer.rewind();
-                    result.append(StandardCharsets.UTF_8.decode(buffer).toString());
-                    buffer.clear();
+                return result.toString();
+            }
+        }
+        return null;
+    }
+
+    public String getContentWithoutUnicode() {
+        if (this.file.exists()) {
+            synchronized (this) {
+                int data = 0;
+                StringBuilder result = new StringBuilder();
+                try (FileChannel channel = FileChannel.open(this.file.toPath(), StandardOpenOption.READ)) {
+                    int bufferSize = 1024;
+                    if (bufferSize > channel.size()) {
+                        bufferSize = (int) channel.size();
+                    }
+                    ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+                    while (channel.read(buffer) > 0) {
+                        int limit = buffer.position();
+                        buffer.limit(limit);
+                        buffer.rewind();
+                        for (int i = 0; i < limit; i++) {
+                            data = buffer.get();
+                            if (data == 13 || data == 10 || data > 90) { //if (data == 13 || data == 10 || data > 90)
+                                result.append((char) data);
+                            }
+                        }
+                        buffer.clear();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+                return result.toString();
             }
-            return result.toString();
+        }
+        return null;
+    }
+
+    public void saveContent(String content) {
+        synchronized (this) {
+            if (!file.exists()) {
+                try {
+                    Files.createFile(this.file.toPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (file.exists()) {
+                try (FileChannel channel = FileChannel.open(file.toPath(),
+                        StandardOpenOption.APPEND)) {
+                    ByteBuffer buffer = ByteBuffer.wrap(content.getBytes());
+                    channel.write(buffer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
-    public static String getContentWithoutUnicode(String file) throws IOException {
-        File f = new File(file);
-        InputStream i = new FileInputStream(f);
-        String output = "";
-        int data;
-        while ((data = i.read()) > 0) {
-            if (data < 0x80) {
-                output += (char) data;
-            }
-        }
-        return output;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParseFile parseFile = (ParseFile) o;
+        return Objects.equals(file, parseFile.file);
     }
 
-    public static void saveContent(String content, File file) {
-        try (OutputStream o = new FileOutputStream(file)) {
-
-            for (int i = 0; i < content.length(); i += 1) {
-                o.write(content.charAt(i));
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+    @Override
+    public int hashCode() {
+        return Objects.hash(file);
     }
 }
--- src/test/java/ru/job4j/concurrent/ParseFileTest.java
+++ src/test/java/ru/job4j/concurrent/ParseFileTest.java
@@ -8,28 +8,61 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 public class ParseFileTest {
     private String src = "src/main/resources/expectedBigTextDescending.txt";
     private String srcUnicode = "src/main/resources/expectedBigTextDescendingWithUnicode.txt";
-    private File input = new File(src);
+    private String tmp = "src/main/resources/tmp.txt";
+    String content = src + srcUnicode + tmp;
+
     @Test
     public void whenGetContentThenGetIt() throws IOException {
+        File file = new File(src);
+        ParseFile parseFile = ParseFile.of(file);
         StringBuilder expected = new StringBuilder();
-        Files.readAllLines(Path.of(src)).forEach(s->expected.append(s).append(System.lineSeparator()));
+        Files.readAllLines(Path.of(src)).forEach(s -> expected.append(s).append(System.lineSeparator()));
         expected.delete(expected.lastIndexOf(System.lineSeparator()), expected.length());
-        String result = ParseFile.getContent(src);
+        String result = parseFile.getContent();
         assertThat(result, is(expected.toString()));
     }
 
     @Test
     public void whenGetContentWithUnicodeThenGetTextWithoutIt() throws IOException {
+        File unicode = new File(srcUnicode);
+        ParseFile parseFile = ParseFile.of(unicode);
         StringBuilder expected = new StringBuilder();
-        Files.readAllLines(Path.of(src)).forEach(s->expected.append(s).append(System.lineSeparator()));
+        Files.readAllLines(Path.of(src)).forEach(s -> expected.append(s).append(System.lineSeparator()));
         expected.delete(expected.lastIndexOf(System.lineSeparator()), expected.length());
-        String result = ParseFile.getContentWithoutUnicode(srcUnicode);
+        String result = parseFile.getContentWithoutUnicode();
+        result = result.replaceFirst("\r\n", "");
         assertThat(result, is(expected.toString()));
     }
 
+    @Test
+    public void whenSaveContentThanFileContentsData() throws IOException {
+
+        File toSave = new File(tmp);
+        assertThat(!toSave.exists(), is(true));
+        ParseFile parseFile =ParseFile.of(toSave);
+        parseFile.saveContent(content);
+        String result = parseFile.getContent();
+        assertThat(result, is(content));
+
+        parseFile.saveContent(content);
+        result = parseFile.getContent();
+        assertThat(result, is(content + content));
+
+        Files.deleteIfExists(toSave.toPath());
+    }
+
+    @Test
+    public void whenFileNotExistsThenGetNull(){
+        File f1= new File("null");
+        ParseFile parseFile = ParseFile.of(f1);
+        assertNull(parseFile.getContent());
+
+        assertNull(parseFile.getContentWithoutUnicode());
+    }
 }
