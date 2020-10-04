09e5f323f22d679ed9e2e930bc9ba2aad1ee6f98
Vasiliy Gasevskiy <gasevskyv@gmail.com>
2020-10-04 20:00:27 +0700

    tasks completed

--- src/main/java/ru/job4j/userstorage/User.java
+++ src/main/java/ru/job4j/userstorage/User.java
@@ -5,37 +5,22 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode
 public class User {
     @EqualsAndHashCode.Include
-    private int id;
-    @Setter
+    private final int id;
+
     @EqualsAndHashCode.Include
+    @Setter
     private int amount;
-    private static volatile int counter = 0;
 
-    public User() {
-        synchronized (User.class) {
-            this.id = counter++;
-        }
-        this.amount = 0;
-    }
-
-    public static User of(User user) {
-        if (user != null) {
-            synchronized (user) { // String.valueOf(user.id)
-                return new User(user.getId(), user.getAmount());
-            }
-        }
-        return null;
-    }
-
-    private User(int id, int amount) {
+    public User(int id, int amount) {
         this.id = id;
         this.amount = amount;
     }
 
-    public static void clear(){
-        counter = 0;
+    public User(int id) {
+        this.id = id;
+        this.amount = 0;
     }
 }
