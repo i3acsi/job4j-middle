8a92c9e09a02a239a5f7bd5ae28c279df585e007
i3acsi <gasevskyv@gmail.com>
2021-02-16 12:08:40 +0700

    applied the wrapper pattern

--- src/main/java/ru/job4j/hibernate/myitems/Store.java
+++ src/main/java/ru/job4j/hibernate/myitems/Store.java
@@ -2,12 +2,14 @@ package ru.job4j.hibernate.myitems;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class Store {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -15,36 +17,41 @@ public class Store {
     private final SessionFactory sf = new MetadataSources(registry)
             .buildMetadata().buildSessionFactory();
 
+    @SuppressWarnings("unchecked")
     public List<MyItem> findAll(boolean isDone) {
-        List<MyItem> result = null;
-        try (Session session = sf.openSession()) {
+        return tx(session -> {
             if (!isDone) {
-                result = session.createQuery("FROM " + MyItem.class.getSimpleName()).getResultList();
+                return session.createQuery("FROM " + MyItem.class.getSimpleName() + " i ORDER BY i.id ASC").getResultList();
             } else {
-                result = session.createQuery("FROM " + MyItem.class.getSimpleName() + " WHERE done = false ").getResultList();
+                return  session.createQuery("FROM " + MyItem.class.getSimpleName() + " i  WHERE done = false ORDER BY i.id ASC").getResultList();
             }
-        }
-        return result;
+        });
     }
 
     public boolean add(MyItem item) {
-        boolean res = false;
-        try (Session session = sf.openSession()) {
-            int id = (int) session.save(item);
-            res = id > 0;
-        }
-        return res;
+        return tx(session -> ((int) session.save(item))>0);
     }
 
     public boolean makeDone(int id) {
-        int result;
-        try (Session session = sf.openSession()) {
-            session.getTransaction().begin();
+        return tx(session -> {
             Query query = session.createQuery("UPDATE " + MyItem.class.getSimpleName() + " SET done = true WHERE id = :paramId");
             query.setParameter("paramId", id);
-            result = query.executeUpdate();
-            session.getTransaction().commit();
+            return query.executeUpdate() > 0;
+        });
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        T result = null;
+        Transaction tx = null;
+        try (Session session = sf.openSession()) {
+            tx = session.beginTransaction();
+            result = command.apply(session);
+            tx.commit();
+        } catch (final Exception e) {
+            if (tx != null)
+                tx.rollback();
+            throw e;
         }
-        return result > 0;
+        return result;
     }
 }
