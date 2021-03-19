3155382e8c307611079c2e2c93c25bdfa559cd52
i3acsi <gasevskyv@gmail.com>
2021-03-19 15:17:56 +0700

    springDI - @Autowired

--- src/main/java/ru/job4j/di/Main.java
+++ src/main/java/ru/job4j/di/Main.java
@@ -1,22 +1,17 @@
 package ru.job4j.di;
 
-import ru.job4j.hibernate.tracker.ITracker;
-import ru.job4j.hibernate.tracker.Input;
-import ru.job4j.hibernate.tracker.StartUI;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        Context context = new Context();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.scan("ru.job4j.di");
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
 
-        context.reg(ru.job4j.hibernate.tracker.ConsoleInput.class);
-        context.reg(ru.job4j.hibernate.tracker.HbmTracker.class);
-
-        Input input = context.get(ru.job4j.hibernate.tracker.ConsoleInput.class);
-        ITracker tracker = context.get(ru.job4j.hibernate.tracker.HbmTracker.class);
-
-        ru.job4j.hibernate.tracker.StartUI ui = new StartUI(input, tracker, System.out::println);
-
-        ui.init();
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
 
     }
 }
--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,8 +1,15 @@
 package ru.job4j.di;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class StartUI {
 
+    @Autowired
     private Store store;
+
 
     public StartUI(Store store) {
         this.store = store;
